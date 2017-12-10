b98d679c22c79184fa0694e6575e3b7ecd6df66c
Vijay athithya <32112111+vj-09@users.noreply.github.com>
2017-12-10 14:38:34 +0530

    Cleaned base model

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tflearn
 import pickle
 import numpy as np
 import json
@@ -6,13 +7,10 @@ import random
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
-
 intents = 'intents.json'
 with  open(intents)as data:
     train_data = json.load(data)
 set_data = train_data['intents']
-# print set_data
-########################################################################
 #%%%%%%%%%%%        cleaning of data         %%%%%%%%%%%%%%%%%%#
 documents = []
 words = []
@@ -20,44 +18,31 @@ tags = []
 responses = []
 ignore_list = ['?',"'s"]
 for data in set_data:
-    # print data['patterns']
     for pattern in data['patterns']:
-        # print pattern
         w = nltk.word_tokenize(str(pattern))
-        # print w
         words.extend(w)
         documents.append((w,str(data['tag'])))
     tags.append(str(data['tag']))
     responses.extend(str(data['responses']))
 words = [stemmer.stem(w.lower()) for w in words if w  not in ignore_list]
-# words.append('accept')
 words = sorted(list(set(words)))
 classes = list(set(tags))
-    # print words
-# print (len(documents), "documents")
-# print (len(classes), "classes", classes)
-# print (len(words), "unique stemmed words", words)
-###############################################################################
 #%%%%%%%%%%%%      Traing and out data           %%%%%%%%%#
 training = []
 output = []
 output_temp = [0]*len(classes)
-# print output_temp
 for doc in documents:
     bag = []
     doc_words = doc[0]
     doc_words =  [stemmer.stem(word.lower()) for word in doc_words]
-    # print doc_words
     for word in words:
         if word in doc_words:
             bag.append(1)
         else:
             bag.append(0)
-    # print bag
     class_list = [0]*len(classes)
     class_list[
     classes.index(doc[1])] = 1
-    # print class_list
     training.append([bag,class_list])
 random.shuffle(training)
 training = np.array(training)
@@ -65,174 +50,89 @@ print np.shape(training)
 train_x = np.array(list(training[:,0]))
 train_y = np.array(list(training[:,1]))
 print np.shape(train_x),len(train_x[0])
-###########################################################################
-#%%%%%%%%%%% tensorflow veriables and placeholders dec %%%%%%%%%%%%%%%%%#
-# tf.reset_default_graph()
-# net = tflearn.input_data(shape=[None,len(train_x[0])])
-# net = tflearn.fully_connected(net,8)
-# net = tflearn.fully_connected(net,8)
-# net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
-# net = tflearn.regression(net)
-#
-# model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
-# # Start training (apply gradient descent algorithm)
-# model.fit(train_x, train_y, n_epoch=800, batch_size=8, show_metric=True)
-#
-# print model.save('model.tflearn')
-######### above model in vanilla tf #################
-def layer(prev_layer, n_in, n_out):
-    w = tf.Variable(tf.random_normal([n_in, n_out]))
-    b = tf.Variable(tf.random_normal([n_out]))
-    return tf.matmul(prev_layer, w) + b
-
-x = tf.placeholder(dtype=tf.float32, shape=[None,len(train_x[0])])
-y = tf.placeholder(dtype=tf.float32, shape=[None,len(train_y[0])])
-
-h1 = layer(x, len(train_x[0]), 8)
-h2 = layer(h1, 8, 8)
-predicted_y = layer(h2, 8, len(train_y[0]))
-
-# out = tf.nn.softmax(predicted_y)
-
-loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=predicted_y, labels=y))
-train_step = tf.train.GradientDescentOptimizer(1e-3).minimize(loss)
-init = tf.global_variables_initializer()
-
-sess = tf.Session()
-sess.run(init)
-
-for i in range(10000):
-    loss_, _ = sess.run([loss, train_step], feed_dict={x:train_x, y:train_y})
-    if i % 50 == 0:
-        print "i : {}, loss : {}".format(i, loss_)
-
-###################################################################################
 #%%%%%%%%%%%%%% model2 with test %%%%%%%%%%%%%%%%%
-# input_features = len(train_x[0])
-# hidden_nodes1 = 10
-# hidden_nodes2 = 20
-# output_classes = len(train_y[0])
-# ######## input layer to hidden1########
-# #27*47
-# #27*47 47*10 27*10
-# #27*10 10*20 27*20
-# #27*20 20*9 27*9
-# X = tf.placeholder(tf.float32,shape=[None,input_features],name='XXX')
-# Y = tf.placeholder(tf.float32,shape=[None,output_classes],name="YYY")
-# w1 = tf.Variable(tf.random_normal([input_features,hidden_nodes1]))
-# b1 = tf.Variable(tf.random_normal([hidden_nodes1]))
-# l1_out = tf.nn.softmax( tf.matmul(X,w1) + b1 )
-#
-# ########## hidden1 to hidden2########
-# w2 = tf.Variable(tf.random_normal([hidden_nodes1,hidden_nodes2]))
-# b2 = tf.Variable(tf.random_normal([hidden_nodes2]))
-# l2_out = tf.nn.softmax(tf.matmul(l1_out,w2) + b2)
-# ############hidden2 to output ################
-#
-# w3 =  tf.Variable(tf.random_normal([hidden_nodes2,output_classes]))
-# b3 = tf.Variable(tf.random_normal([output_classes]))
-# y = tf.nn.softmax(tf.matmul(l2_out,w3) + b3)
-# y_max = tf.argmax(y,1)
-# cross_entropy = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(y), reduction_indices=[1]))
-# train_step = tf.train.GradientDescentOptimizer(0.005).minimize(cross_entropy)
-#
-# sess = tf.InteractiveSession()
-# tf.global_variables_initializer().run()
-# epochs = 20000
-#
-# for i in range(epochs):
-#     # print train_x[10],train_y[0]
-#     # print sess.run(train_step, feed_dict={ X:train_x, Y:train_y })
-#     cross_ent_ , _ = sess.run([y_max,train_step], feed_dict={ X:train_x, Y:train_y })
-#     if i%100 == 0:
-#         print "training step : {}, cross_entropy : {}".format(i, cross_ent_)
+input_features = len(train_x[0])
+hidden_nodes1 = 10
+hidden_nodes2 = 20
+output_classes = len(train_y[0])
+######## input layer to hidden1########
+#27*47
+#27*47 47*10 27*10
+#27*10 10*20 27*20
+#27*20 20*9 27*9
+X = tf.placeholder(tf.float32,shape=[None,input_features],name='XXX')
+Y = tf.placeholder(tf.float32,shape=[None,output_classes],name="YYY")
+w1 = tf.Variable(tf.random_normal([input_features,hidden_nodes1]))
+b1 = tf.Variable(tf.random_normal([hidden_nodes1]))
+l1_out = tf.nn.softmax( tf.matmul(X,w1) + b1 )
 
+########## hidden1 to hidden2########
+w2 = tf.Variable(tf.random_normal([hidden_nodes1,hidden_nodes2]))
+b2 = tf.Variable(tf.random_normal([hidden_nodes2]))
+l2_out = tf.nn.softmax(tf.matmul(l1_out,w2) + b2)
+############hidden2 to output ################
 
-########### test model2 ###################
-# with  open("test_data.json")as data:
-    # test_data = json.load(data)
-# set_data = ['work']
-# test_document = []
-# for data in set_data:
-#     # print data['patterns']
-#     # print pattern
-#     w1 = nltk.word_tokenize(str(data))
-#     test_words = [stemmer.stem(w.lower()) for w in w1 if w  not in ignore_list]
-#     test_document.append(test_words)
-# test_x = []
-# for doc in test_document:
-#     bag = []
-#     for word in words:
-#         if word in doc:
-#             bag.append(1)
-#         else:
-#             bag.append(0)
-#     test_x.append(bag)
-# print test_document
-# print set_data
-# out = sess.run(y_max, feed_dict={ X:test_x })
-# print classes
-# for i in range(len(out)):
-#     print out[i]
-#     print "data : {} class : [ {} ]".format(set_data[i], classes[out[i]])
-# pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':train_y}, open( "training_data", "wb" ) )
+w3 =  tf.Variable(tf.random_normal([hidden_nodes2,output_classes]))
+b3 = tf.Variable(tf.random_normal([output_classes]))
+y = tf.nn.softmax(tf.matmul(l2_out,w3) + b3)
+y_max = tf.argmax(y,1)
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.005).minimize(cross_entropy)
 
-################### test model ###########
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+epochs = 10000
+
+for i in range(epochs):
+    cross_ent_ , _ = sess.run([y_max,train_step], feed_dict={ X:train_x, Y:train_y })
+    if i%100 == 0:
+        print "training step : {}, cross_entropy : {}".format(i, cross_ent_)
+
+################## test model ###########
 def bow(sentance,words,show_details =False):
     # sentence_words = clean_input_sentance(sentence)
     sentence_words = nltk.word_tokenize(sentance)
     sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
-    bag = [0]*len(words)
-    for s in sentence_words:
-        for i,w in enumerate(words):
-            if w==s:
-                bag[i] = 1
-                if show_details:
-                    print ("found%s"%w)
-    return np.array(bag)
-p = bow("is your shop open today",words)
+    print words,sentence_words
+    t = []
+    for doc in sentence_words:
+        bag = []
+        # print doc
+        for word in words:
+            if word in doc:
+                bag.append(1)
+                print doc
+            else:
 
-
+                bag.append(0)
+        t.append(bag)
+    return t
 #############response model###########
 ERROR_THRESHOLD = 0.25
 context = {}
 
 def classify(sentence):
     return_list=[]
-    # results = model.predict([bow(sentence,words)])[0]
-    results = sess.run(predicted_y, feed_dict={x:[bow(sentence,words)]})[0]
-    # print results
+    return_list = bow(sentence,words)
+    results = sess.run(y_max, feed_dict={ X:return_list})
     results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
-    # print results
     results.sort(key=lambda x: x[1], reverse=True)
-    # print results
     for r in results:
         return_list.append((classes[r[0]], r[1]))
-    # print return_list
     # return tuple of intent and probability
     return return_list
 
 def response(sentence, userID='123', show_details=False):
     results = classify(sentence)
-
-    print "GIVEN : {}".format(sentence)
-    if len(results) == 0:
-        print "No results"
-
+    print results
     # if we have a classification then find the matching intent tag
     if results:
-
-        # loop as long as there are matches to 0process
+         # loop as long as there are matches to process
         for a in results:
-            print a
             for i in set_data:
-                # print i
-                # find a tag matching the first result
+            # find a tag matching the first result
                 if i['tag'] == a[0]:
-                    # print i
-                    # print i['tag'] , results[0][0]
-                    # a random response from the intent
-                    # print sentence, i['responses']
+                    print i['tag'] , results[0][0]
                     if 'context_set' in i:
                         print ('context:',i['context_set'])
                         context[userID] = i['context_set']
@@ -242,4 +142,4 @@ def response(sentence, userID='123', show_details=False):
                             print random.choice(i['responses'])
             results.pop(0)
 response("can we rent a mope")
-response('i like movies')
+response('today')
